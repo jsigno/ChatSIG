6e538c7d5d507a9c121c18b90423086da7d9cf63
Justin Signo <justinsigno@yahoo.com>
2023-03-01 16:01:49 -0500

    done

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:7575', {
+  const response = await fetch('https://chatsig.onrender.com', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
